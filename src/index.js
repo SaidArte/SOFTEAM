f92a6d799ee7d7a6438f36d16dbbd6cbe53ead75
JuanZelayaReconco <juanzelayaunah@outlook.es>
2023-07-15 19:58:45 -0600

    JWT Commit
    Integración de Json Web Token al sistema.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 // constantes requerida en nuestro carpeta 
 
 const express = require('express');
+const jwt = require('jsonwebtoken')
 const app = express();
 
-//conxion puerto
+app.get('/', (req, res) => {
+    res.json({
+        text: 'api work!'
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    const user = {id: 3}
+    const token = jwt.sign({user}, 'my_ultrasecret_token');
+    res.json({
+        token
+    });
+});
+
+app.get('/api/protected', ensureToken, (req, res) => {
+    jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
+        if (err){
+            res.sendStatus(403);
+        }else {
+            res.json({
+                text: 'protected',
+                data
+            });
+        }
+    });
+});
+
+function ensureToken (req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    console.log(bearerHeader);
+    if (typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+//conexion puerto
 
 app.listen(3306, () =>{
     console.log('server on port 3306');
