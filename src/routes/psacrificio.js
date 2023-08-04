60474c618e290d1771f8a6cde0ad3d9075c0354b
Villalobos19 <dmcruzv@unah.hn>
2023-08-03 22:16:30 -0600

    Commit de la api psacrificio
    Se modifico la api de Permiso Sacrificio

--- src/routes/psacrificio.js
+++ src/routes/psacrificio.js
@@ -1,16 +1,16 @@
 // constantes requerida en nuestro carpeta routes para datos del modulo Permisos Sacrificios
 const express = require  ('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 
 const mysqlConnection= require('../database');
 
 //SELECT ALL: Aqui llamaremos a la tabla con todos los registros de la Base de Datos
 router.get('/PSACRIFICIO/GETALL' , (req , res ) =>{
-  jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
+  /*jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
     if (err){
       res.sendStatus(403);
-  }else {
+  }else {*/
         mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO' , ( err, rows , fields ) =>{
         if(!err){
           res.json(rows);
@@ -18,21 +18,20 @@ router.get('/PSACRIFICIO/GETALL' , (req , res ) =>{
           console.log(err);
         }
       });
-    }
- });
+    //}
+ //});
 });
 
 //POST: Insertaremos nuevos registros en la tabla
 router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
-  jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
+  /*jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
     if (err){
        res.sendStatus(403);
-    }else {
+    }else {*/
       const { //Llamaremos a todas las variables del Procedimiento Almacenado PSacrificio
-        TABLA_NOMBRE,
-        TIPO_OPERACION,
+        //TABLA_NOMBRE,
+        //TIPO_OPERACION,
         COD_PSACRIFICIO,
-        FEC_REG_PSACRIFICIO,
         NOM_PERSONA,
         DNI_PERSONA,
         TEL_PERSONA,
@@ -44,14 +43,13 @@ router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
       //Aqui llamamos a los parametros del procedmiento 
       const query =`
 
-       CALL SP_MOD_PERMISOS_SACRIFICIO(?,?,?,?,?,?,?,?,?,?);
+       CALL SP_MOD_PERMISOS_SACRIFICIO('PERMISOS_SACRIFICIO','I',?,?,?,?,?,?,?);
    
     `;
     mysqlConnection.query(query , [
-            TABLA_NOMBRE,
-            TIPO_OPERACION,
+            //TABLA_NOMBRE,
+            //TIPO_OPERACION,
             COD_PSACRIFICIO,
-            FEC_REG_PSACRIFICIO,
             NOM_PERSONA,
             DNI_PERSONA,
             TEL_PERSONA,
@@ -67,16 +65,16 @@ router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
             }
 
       });
-    }
-  });
+    //}
+  //});
 });
 
 //SELECT ONE: Seleccionamos un registro en especifico mediante nuestra llave primaria
 router.get('/PSACRIFICIO/GETONE/:COD_PSACRIFICIO',(req, res) =>{
-  jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
+  /*jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
     if (err){
       res.sendStatus(403);
-   }else {
+   }else {*/
       const { COD_PSACRIFICIO } = req.params;
       mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO WHERE COD_PSACRIFICIO = ?', [COD_PSACRIFICIO], (err, 
          rows, fields) => {
@@ -86,20 +84,20 @@ router.get('/PSACRIFICIO/GETONE/:COD_PSACRIFICIO',(req, res) =>{
              console.log(err);
              }
        });
-    }
- });
+    //}
+ //});
 });
 
 //PUT: Actualizamos cualquier registro llamando al COD_PSACRIFICIO
 router.put('/PSACRIFICIO/ACTUALIZAR/:COD_PSACRIFICIO' , (req , res)=>{
-  jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
+  /*jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
    if (err){
         res.sendStatus(403);
-   }else {
+   }else {*/
+   try {
         const {  
-          TABLA_NOMBRE,
-          TIPO_OPERACION,
-          FEC_REG_PSACRIFICIO,
+          //TABLA_NOMBRE,
+          //TIPO_OPERACION,
           NOM_PERSONA,
           DNI_PERSONA,
           TEL_PERSONA,
@@ -109,27 +107,22 @@ router.put('/PSACRIFICIO/ACTUALIZAR/:COD_PSACRIFICIO' , (req , res)=>{
       } =req.body;
       const {COD_PSACRIFICIO} = req.params;
       const query = `
-      CALL SP_MOD_PERMISOS_SACRIFICIO(?,?,?,?,?,?,?,?,?,?)`;
-      mysqlConnection.query(query , [ 
-          TABLA_NOMBRE,
-          TIPO_OPERACION,
-          COD_PSACRIFICIO,
-          FEC_REG_PSACRIFICIO,
-          NOM_PERSONA,
-          DNI_PERSONA,
-          TEL_PERSONA,
-          FEC_SACRIFICIO,
-          COD_ANIMAL,
-          DIR_PSACRIFICIO 
-          ] , (err , rows , fields) =>{
+      CALL SP_MOD_PERMISOS_SACRIFICIO
+      ('PERMISOS_SACRIFICIO','U','${COD_PSACRIFICIO}',
+      '${NOM_PERSONA}','${DNI_PERSONA}','${TEL_PERSONA}','${FEC_SACRIFICIO}','${COD_ANIMAL}','${DIR_PSACRIFICIO}')`;
+      mysqlConnection.query(query , (err , result) =>{
         if(!err){
           res.json({Status: 'Datos actualizados'});
         }else{
             console.log(err);
+            res.status(500).json({ error: 'Error al actualizar los datos'});
         }
       });
-     }
-    });
+     } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Datos inválidos'});
+     } 
+    //});
   });
 
 module.exports = router;
