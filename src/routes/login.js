1f761f185af6d1917edd4f3c8e456520a3d530d9
JuanZelayaReconco <juanzelayaunah@outlook.es>
2023-08-15 12:27:47 -0600

    Commit user
    Se agregó una funcionalidad para que el API retorne todas las columnas solicitadas por el "select".

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,14 +10,14 @@ router.post('/api/login', (req, res) => {
     const { NOM_USUARIO, PAS_USUARIO } = req.body;
   
     // Consulta SQL para verificar si las credenciales del usuario son correctas.
-    const query = `SELECT * FROM TBL_MS_USUARIOS WHERE NOM_USUARIO = ? AND PAS_USUARIO = ?;`;
+    const query = `SELECT a.COD_USUARIO, a.NOM_USUARIO, b.NOM_PERSONA, c.NOM_ROL, a.IND_USUARIO, a.FEC_ULTIMO_ACCESO, a.LIM_INTENTOS, a.NUM_INTENTOS_FALLIDOS, a.FEC_VENCIMIENTO FROM TBL_MS_USUARIOS a, PERSONAS  b, TBL_MS_ROLES c WHERE a.COD_PERSONA = b.COD_PERSONA AND a.COD_ROL = c.COD_ROL AND NOM_USUARIO = ? AND PAS_USUARIO = ?;`;
     mysqlConnection.query(query, [NOM_USUARIO, PAS_USUARIO], (error, results) => {
         if (error) {
             res.status(500).json({ error: 'Error interno del servidor' });
         } else {
             if (results.length > 0) {
                 if (results[0].IND_USUARIO === 'ACTIVO') {
-                    res.json({ message: 'Inicio de sesión exitoso', NOM_USUARIO: results[0].NOM_USUARIO });
+                    res.json({ message: 'Inicio de sesión exitoso', user: results[0]});
                 } else {
                     res.status(401).json({ error_type: 'inactive', message: 'Usuario inactivo' });
                 }
