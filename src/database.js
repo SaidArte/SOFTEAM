145f8c84aeaae201d00eae071d06ed660e842bb1
JuanZelayaReconco <juanzelayaunah@outlook.es>
2023-08-24 18:25:28 -0600

    Commit DB railway
    Añadiendo conexion a railway

--- src/database.js
+++ src/database.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql');
+
  
 //const  mysqlConnection = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host:'142.44.161.115',
-    user: 'SOFTEAM',
-    port: 3306,
-    password: 'New##28yu',
-    database: 'SOFTEAM'
+    host:'containers-us-west-65.railway.app',
+    user: 'root',
+    port: 5536,
+    password: '5yPcDH1YRIXyR1j75pbZ',
+    database: 'railway'
 
 });
 
 mysqlConnection.connect( function (err){
      if(!err){
-        console.log('¡Conexión exitosa en el puerto 3000!');
+        console.log(`¡Conexión exitosa en el puerto!`);
      }else{
         console.log('No se ha podido establecer conexión con la base de datos.');
      }
