56845ed1731ff3d1c31e873abb0df486f98ef045
SaidArte <ivis.arteaga@unah.hn>
2023-08-26 18:28:43 -0600

    cambios
    commit pruebas

--- src/database.js
+++ src/database.js
@@ -3,19 +3,21 @@ const mysql = require('mysql2');
 //const  mysqlConnection = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
+
    host: '82.180.133.39',
    port: 3306,
    user: 'soft',
    password: 'Arteaga1234.',
    database: 'SOFTEAM',
-   
+   multipleStatements: true
  });
+
 
 mysqlConnection.connect( function (err){
      if(!err){
         console.log('¡Conexión exitosa en el puerto 4000!');
      }else{
-        console.log('No se ha podido establecer conexión con la base de datos.');
+        console.log(err);
      }
 
 });
