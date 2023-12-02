84f07903552a5803a9038b959e9480beb47c54fd
Villalobos19 <117198123+Villalobos19@users.noreply.github.com>
2023-12-02 01:46:03 -0600

    Commit
    Apis

--- src/routes/psacrificio.js
+++ src/routes/psacrificio.js
@@ -1,33 +1,33 @@
 // constantes requerida en nuestro carpeta routes para datos del modulo Permisos Sacrificios
-const express = require  ('express');
+const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
-const mysqlConnection= require('../database');
+const mysqlConnection = require('../database');
 
 //SELECT ALL: Aqui llamaremos a la tabla con todos los registros de la Base de Datos
-router.get('/PSACRIFICIO/GETALL' , (req , res ) =>{
+router.get('/PSACRIFICIO/GETALL', (req, res) => {
   jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
-    if (err){
+    if (err) {
       res.sendStatus(403);
-  }else {
-        mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO' , ( err, rows , fields ) =>{
-        if(!err){
+    } else {
+      mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO', (err, rows, fields) => {
+        if (!err) {
           res.json(rows);
-        }else{
+        } else {
           console.log(err);
         }
       });
     }
- });
+  });
 });
 
 //POST: Insertaremos nuevos registros en la tabla
-router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
+router.post('/PSACRIFICIO/INSERTAR', (req, res) => {
   jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
-    if (err){
-       res.sendStatus(403);
-    }else {
+    if (err) {
+      res.sendStatus(403);
+    } else {
       const { //Llamaremos a todas las variables del Procedimiento Almacenado PSacrificio
         //TABLA_NOMBRE,
         //TIPO_OPERACION,
@@ -38,33 +38,33 @@ router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
         FEC_SACRIFICIO,
         DIR_PSACRIFICIO,
         ANIMAL,
-        DET_ANIMAL
-        } =req.body;
+        COL_ANIMAL
+      } = req.body;
       console.log(req.body)
       //Aqui llamamos a los parametros del procedmiento 
-      const query =`
+      const query = `
 
        CALL SP_MOD_PERMISOS_SACRIFICIO('PERMISOS_SACRIFICIO','I',?,?,?,?,?,?,?,?);
    
     `;
-    mysqlConnection.query(query , [
-            //TABLA_NOMBRE,
-            //TIPO_OPERACION,
-            COD_PSACRIFICIO,
-            NOM_PERSONA,
-            DNI_PERSONA,
-            TEL_PERSONA,
-            FEC_SACRIFICIO,
-            DIR_PSACRIFICIO,
-            ANIMAL,
-            DET_ANIMAL
-            //Aqui mostramos un mensaje si el procedimiento fue realizado correctamento o muestra un mensaje de error
-            ] , (err , rows , fields) =>{
-            if(!err){
-              res.json({Status: 'Permiso de Sacrificio Registrado'});
-            }else{
-                console.log(err);
-            }
+      mysqlConnection.query(query, [
+        //TABLA_NOMBRE,
+        //TIPO_OPERACION,
+        COD_PSACRIFICIO,
+        NOM_PERSONA,
+        DNI_PERSONA,
+        TEL_PERSONA,
+        FEC_SACRIFICIO,
+        DIR_PSACRIFICIO,
+        ANIMAL,
+        COL_ANIMAL
+        //Aqui mostramos un mensaje si el procedimiento fue realizado correctamento o muestra un mensaje de error
+      ], (err, rows, fields) => {
+        if (!err) {
+          res.json({ Status: 'Permiso de Sacrificio Registrado' });
+        } else {
+          console.log(err);
+        }
 
       });
     }
@@ -72,32 +72,32 @@ router.post('/PSACRIFICIO/INSERTAR' , (req , res )=>{
 });
 
 //SELECT ONE: Seleccionamos un registro en especifico mediante nuestra llave primaria
-router.get('/PSACRIFICIO/GETONE/:COD_PSACRIFICIO',(req, res) =>{
+router.get('/PSACRIFICIO/GETONE/:COD_PSACRIFICIO', (req, res) => {
   jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
-    if (err){
+    if (err) {
       res.sendStatus(403);
-   }else {
+    } else {
       const { COD_PSACRIFICIO } = req.params;
-      mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO WHERE COD_PSACRIFICIO = ?', [COD_PSACRIFICIO], (err, 
-         rows, fields) => {
-         if(!err){
-             res.json(rows);
-             }else{
-             console.log(err);
-             }
-       });
+      mysqlConnection.query('SELECT * FROM PERMISOS_SACRIFICIO WHERE COD_PSACRIFICIO = ?', [COD_PSACRIFICIO], (err,
+        rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log(err);
+        }
+      });
     }
- });
+  });
 });
 
 //PUT: Actualizamos cualquier registro llamando al COD_PSACRIFICIO
-router.put('/PSACRIFICIO/ACTUALIZAR/:COD_PSACRIFICIO' , (req , res)=>{
+router.put('/PSACRIFICIO/ACTUALIZAR/:COD_PSACRIFICIO', (req, res) => {
   jwt.verify(req.token, 'my_ultrasecret_token', (err, data) => {
-   if (err){
-        res.sendStatus(403);
-   }else {
-   try {
-        const {  
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      try {
+        const {
           //TABLA_NOMBRE,
           //TIPO_OPERACION,
           NOM_PERSONA,
@@ -106,28 +106,28 @@ router.put('/PSACRIFICIO/ACTUALIZAR/:COD_PSACRIFICIO' , (req , res)=>{
           FEC_SACRIFICIO,
           DIR_PSACRIFICIO,
           ANIMAL,
-          DET_ANIMAL
+          COL_ANIMAL
 
-      } =req.body;
-      const {COD_PSACRIFICIO} = req.params;
-      const query = `
+        } = req.body;
+        const { COD_PSACRIFICIO } = req.params;
+        const query = `
       CALL SP_MOD_PERMISOS_SACRIFICIO
       ('PERMISOS_SACRIFICIO','U','${COD_PSACRIFICIO}',
-      '${NOM_PERSONA}','${DNI_PERSONA}','${TEL_PERSONA}','${FEC_SACRIFICIO}','${DIR_PSACRIFICIO}','${ANIMAL}', '${DET_ANIMAL}')`;
-      mysqlConnection.query(query , (err , result) =>{
-        if(!err){
-          res.json({Status: 'Datos actualizados'});
-        }else{
+      '${NOM_PERSONA}','${DNI_PERSONA}','${TEL_PERSONA}','${FEC_SACRIFICIO}','${DIR_PSACRIFICIO}','${ANIMAL}', '${COL_ANIMAL}')`;
+        mysqlConnection.query(query, (err, result) => {
+          if (!err) {
+            res.json({ Status: 'Datos actualizados' });
+          } else {
             console.log(err);
-            res.status(500).json({ error: 'Error al actualizar los datos'});
-        }
-      });
-     } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Datos inválidos'});
-     }
-    } 
-    });
+            res.status(500).json({ error: 'Error al actualizar los datos' });
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: 'Datos inválidos' });
+      }
+    }
   });
+});
 
 module.exports = router;
